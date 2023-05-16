269c39ae4ec844f67c314dc51f5b3d0dc90b11d8
Julek Jakóbik <jacekeo007@gmail.com>
2023-05-16 12:30:58 +0200

    Navbar STYLED

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 //REACT COMPONENTS
-import { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import { NavLink, Link } from 'react-router-dom';
 //COMPONENT CSS
 import './Navbar.css';
 // REACT ICONS
@@ -11,28 +11,44 @@ import { GiHamburgerMenu } from 'react-icons/gi';
 const Navbar = () => {
 
     const [menu ,setMenu] = useState(false)
+    const activeLink = 'text-primary';
+    const normalLink = 'text-black-600';
 
-  
+    // useEffect(()=>{
+    //     LinkSlider('45px');
+    // });
+    
+    
+    const LinkSlider = (px) => {
+        // px.preventDefault();
+        const bar = document.querySelector('.animation');
+        bar.style.left = `${px}`
+     
+
+
+    } 
 
     return ( 
-        <>
+        <div className='Navbar'>
             {/* NAV BIG SCREEN */}
-            <nav className='flex justify-center items-center flex-col text-center lg:flex-row md:justify-around md:py-[23px] content-center tracking-wider' >
-                
-                <div className="flex justift-center items-center flex-row py-[15px] md:py-[0]">
-                    <div className='logo-box flex justify-center items-center flex-row text-[20px] sm:text-[25px] md:text-[34px]'>
-                        <p>J</p>
-                        <p className='logo'>J</p>
+                <nav className='flex justify-center items-center flex-col text-center lg:flex-row md:justify-around md:py-[23px] content-center tracking-wider' >
+                    
+                    <div className="flex justift-center items-center flex-row py-[15px]">
+                        <div className='logo-box flex justify-center items-center flex-row text-[20px] sm:text-[25px] md:text-[34px]'>
+                            <p>J</p>
+                            <p className='logo'>J</p>
+                        </div>
+                        <h1 className='text-[17px]  ml-[20px] sm:text-[16px] sm:ml-[10px] md:text-[19px] md:ml-[20px]'>Juliusz Jakóbik</h1>
                     </div>
-                    <h1 className='text-[17px]  ml-[20px] sm:text-[16px] sm:ml-[10px] md:text-[19px] md:ml-[20px]'>Juliusz Jakóbik</h1>
-                </div>
-                
-                <ul className='flex justify-center items-center  flex-col text-center hidden md:flex md:flex-row '>
-                    <Link to="/"><li className='px-[20px] py-[5px] mx-[30px] hover:border-b-[2px] hover:border-primary hover:mt-[2px] active:text-primary cursor-pointer'>home</li></Link>
-                    <Link to="/portfolio"><li className='px-[20px] py-[5px] mx-[30px] hover:border-b-[2px] hover:border-primary hover:mt-[2px] cursor-pointer'>portfolio</li></Link>
-                    <Link to="/contactcv"><li className='px-[20px] py-[5px] mx-[30px] hover:border-b-[2px] hover:border-primary hover:mt-[2px] cursor-pointer'>contact & cv</li></Link>
-                </ul>
-            </nav>
+
+                    <div className="links className='flex justify-center items-center  flex-col text-center hidden py-[15px] md:flex md:flex-row">
+                        <NavLink to="/" className={({ isActive }) => isActive ? activeLink : normalLink} onClick={(e) => LinkSlider('45px')}>home</NavLink>
+                        <NavLink to="/portfolio" className={({ isActive }) => isActive ? activeLink : normalLink} onClick={(e) => LinkSlider('240px')} >portfolio</NavLink>
+                        <NavLink to="/contactcv" className={({ isActive }) => isActive ? activeLink : normalLink} onClick={(e) => LinkSlider('435px')}>contact & cv</NavLink>
+                        <div className="animation start-home"></div>
+                    </div>
+
+                </nav>
 
 
             {/* BURGER BUTTON */}
@@ -52,8 +68,10 @@ const Navbar = () => {
                     <Link to="/contactcv"><li className='hover:border-l-[5px] hover:border-primary hover:mr-[5px] py-[19px] cursor-pointer'>Contact & CV</li></Link>
                     </ul>
                 </div>
-        </>
+        </div>
      );
 }
 
 export default Navbar;
+        
+            
