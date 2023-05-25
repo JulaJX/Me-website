d3ed097d424b98695e313b60f1d0712b8f76ee2c
Julek Jakóbik <jacekeo007@gmail.com>
2023-05-25 23:01:42 +0200

    Home small visual fixes

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -33,30 +33,30 @@ const Skills = () => {
                                 
                                 <Link target="_blank" to="https://en.wikipedia.org/wiki/HTML">
                                     <div className='hover:translate-y-[-8px] ease-in-out duration-200 cursor-pointer'>
-                                        <div className="flex flex-col items-center justify-center bg-primary w-[90px] py-[7px]  md:w-[105px] md:py-[15px] relative rounded-xl">
+                                        <div className="flex flex-col items-center justify-center bg-primary w-[90px] py-[7px]  md:w-[95px] md:py-[12px] relative rounded-xl">
                                             <AiFillHtml5 className=' text-[35px] md:text-[40px] text-[#E44D26]'/> 
                                             <h1 className='text-[12px] text-[#E44D26] tracking-wider mt-[5px] font-bold'>HTML5</h1>
-                                            <div className='bg-secondary absolute w-[90px] h-[70px] md:w-[105px] md:h-[90px] z-[-1] translate-y-[10px] translate-x-[-10px] rounded-xl shd '></div>
+                                            <div className='bg-secondary absolute w-[90px] h-[70px] md:w-[95px] md:h-[80px] z-[-1] translate-y-[10px] translate-x-[-10px] rounded-xl shd '></div>
                                         </div>
                                     </div>
                                 </Link>
                                 
                                 <Link target="_blank" to="https://en.wikipedia.org/wiki/CSS">
                                     <div className='hover:translate-y-[-8px] ease-in-out duration-200 cursor-pointer'>
-                                        <div className="flex flex-col items-center justify-center bg-primary w-[90px] py-[7px]  md:w-[105px] md:py-[15px] relative rounded-xl">
+                                        <div className="flex flex-col items-center justify-center bg-primary w-[90px] py-[7px]  md:w-[95px] md:py-[12px] relative rounded-xl">
                                             <SiCss3 className='text-[35px] md:text-[40px] text-[#33A9DC]'/> 
                                             <h1 className='text-[12px] text-[#33A9DC] tracking-wider mt-[5px] font-bold'>CSS3</h1>
-                                            <div className='bg-secondary absolute w-[90px] h-[70px] md:w-[105px] md:h-[90px] z-[-1] translate-y-[10px] translate-x-[-10px] rounded-xl shd '></div>
+                                            <div className='bg-secondary absolute w-[90px] h-[70px] md:w-[95px] md:h-[80px] z-[-1] translate-y-[10px] translate-x-[-10px] rounded-xl shd '></div>
                                         </div>
                                     </div>
                                 </Link>
                                 
                                 <Link target="_blank" to="https://en.wikipedia.org/wiki/Sass_(style_sheet_language)">
                                     <div className='hover:translate-y-[-8px] ease-in-out duration-200 cursor-pointer'>
-                                        <div className="flex flex-col items-center justify-center bg-primary w-[90px] py-[7px]  md:w-[105px] md:py-[15px] relative rounded-xl">
+                                        <div className="flex flex-col items-center justify-center bg-primary w-[90px] py-[7px]  md:w-[95px] md:py-[12px] relative rounded-xl">
                                             <SiSass className='text-[35px] md:text-[40px] text-[#CD6799]'/> 
                                             <h1 className='text-[12px] text-[#CD6799] md:text-[14px] tracking-wider mt-[5px] font-bold'>Sass</h1>
-                                            <div className='bg-secondary absolute w-[90px] h-[70px] md:w-[105px] md:h-[90px] z-[-1] translate-y-[10px] translate-x-[-10px] rounded-xl shd '></div>
+                                            <div className='bg-secondary absolute w-[90px] h-[70px] md:w-[95px] md:h-[80px] z-[-1] translate-y-[10px] translate-x-[-10px] rounded-xl shd '></div>
                                         </div>
                                     </div>
                                 </Link>
@@ -64,30 +64,30 @@ const Skills = () => {
 
                                 <Link target="_blank" to="https://en.wikipedia.org/wiki/Tailwind_CSS">
                                     <div className='hover:translate-y-[-8px] ease-in-out duration-200 cursor-pointer'>                                
-                                        <div className="flex flex-col items-center justify-center bg-primary w-[90px] py-[7px]  md:w-[105px] md:py-[15px] relative rounded-xl">
+                                        <div className="flex flex-col items-center justify-center bg-primary w-[90px] py-[7px]  md:w-[95px] md:py-[12px] relative rounded-xl">
                                             <SiTailwindcss className='text-[35px] md:text-[40px] text-[#38BDF8]'/> 
                                             <h1 className='text-[12px] text-[#38BDF8] md:text-[14px] tracking-wider mt-[5px] font-bold'>Tailwind</h1>
-                                            <div className='bg-secondary absolute w-[90px] h-[70px] md:w-[105px] md:h-[90px] z-[-1] translate-y-[10px] translate-x-[-10px] rounded-xl shd '></div>
+                                            <div className='bg-secondary absolute w-[90px] h-[70px] md:w-[95px] md:h-[80px] z-[-1] translate-y-[10px] translate-x-[-10px] rounded-xl shd '></div>
                                         </div>
                                     </div>
                                 </Link>
                                 
                                 <Link target="_blank" to="https://en.wikipedia.org/wiki/JavaScript">
                                 <div className='hover:translate-y-[-8px] ease-in-out duration-200 cursor-pointer'>
-                                    <div className="flex flex-col items-center justify-center bg-primary w-[90px] py-[7px]  md:w-[105px] md:py-[15px] relative rounded-xl">
+                                    <div className="flex flex-col items-center justify-center bg-primary w-[90px] py-[7px]  md:w-[95px] md:py-[13px] relative rounded-xl">
                                         <SiJavascript className='text-[35px] md:text-[40px] text-[F7DF1E]'/> 
-                                        <h1 className='text-[10px] md:text-[14px] text-[F7DF1E] tracking-wide mt-[5px] font-bold'>JavaScript </h1>
-                                        <div className='bg-secondary absolute w-[90px] h-[70px] md:w-[105px] md:h-[90px] z-[-1] translate-y-[10px] translate-x-[-10px] rounded-xl shd '></div>
+                                        <h1 className='text-[10px] md:text-[13px] text-[F7DF1E] mt-[5px] font-bold'>JavaScript</h1>
+                                        <div className='bg-secondary absolute w-[90px] h-[70px] md:w-[95px] md:h-[80px] z-[-1] translate-y-[10px] translate-x-[-10px] rounded-xl shd '></div>
                                     </div>
                                 </div>
                                 </Link>
                                 
                                 <Link target="_blank" to="https://en.wikipedia.org/wiki/React_(software)">
                                     <div className='hover:translate-y-[-8px] ease-in-out duration-200 cursor-pointer'>
-                                        <div className="flex flex-col items-center justify-center bg-primary w-[90px] py-[7px]  md:w-[105px] md:py-[15px] relative rounded-xl">
+                                        <div className="flex flex-col items-center justify-center bg-primary w-[90px] py-[7px]  md:w-[95px] md:py-[12px] relative rounded-xl">
                                             <SiReact className='text-[35px] md:text-[40px] text-[#61DBFB]'/> 
                                             <h1 className='text-[12px] text-[#61DBFB] md:text-[14px] tracking-wider mt-[5px] font-bold'>React</h1>
-                                            <div className='bg-secondary absolute w-[90px] h-[70px] md:w-[105px] md:h-[90px] z-[-1] translate-y-[10px] translate-x-[-10px] rounded-xl shd '></div>
+                                            <div className='bg-secondary absolute w-[90px] h-[70px] md:w-[95px] md:h-[80px] z-[-1] translate-y-[10px] translate-x-[-10px] rounded-xl shd '></div>
                                         </div>
                                     </div>
                                 </Link>
@@ -95,40 +95,40 @@ const Skills = () => {
 
                                 <Link target="_blank" to="https://en.wikipedia.org/wiki/PHP">
                                     <div className='hover:translate-y-[-8px] ease-in-out duration-200 cursor-pointer'>
-                                        <div className="flex flex-col items-center justify-center bg-primary w-[90px] py-[7px]  md:w-[105px] md:py-[15px] relative rounded-xl">
+                                        <div className="flex flex-col items-center justify-center bg-primary w-[90px] py-[7px]  md:w-[95px] md:py-[12px] relative rounded-xl">
                                             <SiPhp className='text-[35px] md:text-[40px] text-[#787CB4]'/> 
                                             <h1 className='text-[12px] text-[#787CB4] md:text-[14px] tracking-wider mt-[5px] font-bold'>PHP</h1>
-                                            <div className='bg-secondary absolute w-[90px] h-[70px] md:w-[105px] md:h-[90px] z-[-1] translate-y-[10px] translate-x-[-10px] rounded-xl shd '></div>
+                                            <div className='bg-secondary absolute w-[90px] h-[70px] md:w-[95px] md:h-[80px] z-[-1] translate-y-[10px] translate-x-[-10px] rounded-xl shd '></div>
                                         </div>
                                     </div>
                                 </Link>
 
                                 <Link target="_blank" to="https://en.wikipedia.org/wiki/MySQL">
                                     <div className='hover:translate-y-[-8px] ease-in-out duration-200 cursor-pointer'>
-                                        <div className="flex flex-col items-center justify-center bg-primary w-[90px] py-[7px]  md:w-[105px] md:py-[15px] relative rounded-xl">
+                                        <div className="flex flex-col items-center justify-center bg-primary w-[90px] py-[7px]  md:w-[95px] md:py-[12px] relative rounded-xl">
                                             <TbBrandMysql className='text-[35px] md:text-[40px] text-[#E48E00]'/> 
                                             <h1 className='text-[12px] text-[#E48E00] md:text-[14px] tracking-wider mt-[5px] font-bold'>mySQL</h1>
-                                            <div className='bg-secondary absolute w-[90px] h-[70px] md:w-[105px] md:h-[90px] z-[-1] translate-y-[10px] translate-x-[-10px] rounded-xl shd '></div>
+                                            <div className='bg-secondary absolute w-[90px] h-[70px] md:w-[95px] md:h-[80px] z-[-1] translate-y-[10px] translate-x-[-10px] rounded-xl shd '></div>
                                         </div>
                                     </div>
                                 </Link>
                                 
                                 <Link target="_blank" to="https://en.wikipedia.org/wiki/Firebase">
                                     <div className='hover:translate-y-[-8px] ease-in-out duration-200 cursor-pointer'>
-                                        <div className="flex flex-col items-center justify-center bg-primary w-[90px] py-[7px]  md:w-[105px] md:py-[15px] relative rounded-xl">
+                                        <div className="flex flex-col items-center justify-center bg-primary w-[90px] py-[7px]  md:w-[95px] md:py-[12px] relative rounded-xl">
                                             <SiFirebase className='text-[35px] md:text-[40px] text-[#F88C12]'/> 
                                             <h1 className='text-[12px] text-[#F88C12] md:text-[14px] tracking-wider mt-[5px] font-bold'>Firebase</h1>
-                                            <div className='bg-secondary absolute w-[90px] h-[70px] md:w-[105px] md:h-[90px] z-[-1] translate-y-[10px] translate-x-[-10px] rounded-xl shd '></div>
+                                            <div className='bg-secondary absolute w-[90px] h-[70px] md:w-[95px] md:h-[80px] z-[-1] translate-y-[10px] translate-x-[-10px] rounded-xl shd '></div>
                                         </div>
                                     </div>
                                 </Link>
 
                                 <Link target="_blank" to="https://en.wikipedia.org/wiki/Vite_(software)">
                                     <div className='hover:translate-y-[-8px] ease-in-out duration-200 cursor-pointer'>
-                                        <div className="flex flex-col items-center justify-center bg-primary w-[90px] py-[7px]  md:w-[105px] md:py-[15px] relative rounded-xl">
+                                        <div className="flex flex-col items-center justify-center bg-primary w-[90px] py-[7px]  md:w-[95px] md:py-[12px] relative rounded-xl">
                                             <SiVitess className='text-[35px] md:text-[40px] text-[#B939FE]'/> 
                                             <h1 className='text-[12px] text-[#B939FE] md:text-[14px] tracking-wider mt-[5px] font-bold'>Vite</h1>
-                                            <div className='bg-secondary absolute w-[90px] h-[70px] md:w-[105px] md:h-[90px] z-[-1] translate-y-[10px] translate-x-[-10px] rounded-xl shd '></div>
+                                            <div className='bg-secondary absolute w-[90px] h-[70px] md:w-[95px] md:h-[80px] z-[-1] translate-y-[10px] translate-x-[-10px] rounded-xl shd '></div>
                                         </div>
                                     </div>
                                 </Link>
--- src/pages/portfolio/Portfolio.jsx
+++ src/pages/portfolio/Portfolio.jsx
@@ -1,6 +1,18 @@
+//IMPORT STYLES
+import './Portfolio.css';
+//IMPORT PHOTO
+import  maze  from '../../assets/maze.png';
+
+
+
 const Portfolio = () => {
+
     return ( 
-        <div className="Portfolio">
+        <div className="Portfolio w-[80%] mx-auto  mt-[200px] flex items-center justify-center">
+            <div className="h-[400px] w-[280px] ">
+            <img src={maze} alt="maze card" className='' />
+
+            </div>
             
         </div>
      );
