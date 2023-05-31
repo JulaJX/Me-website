94df5e8ac21a51ed05ee11bb6e78dd029409b62f
Julek Jakóbik <jacekeo007@gmail.com>
2023-05-31 17:37:17 +0200

    Added few things on project page

--- src/pages/portfolio/Carousel.jsx
+++ src/pages/portfolio/Carousel.jsx
@@ -95,7 +95,7 @@ const Carousel = ({img}) => {
       };
 
     return ( 
-        <div className='Carousel max-w-[450px] h-[350px] xl:max-w-[700px] xl:h-[480px] w-full m-auto py-16 px-4 relative group'>
+        <div className='Carousel  h-[350px] xl:w-[800px] xl:h-[420px]  relative group'>
            
 
             {img == '1' && ( <div style={{ backgroundImage: `url(${slides[currentIndex].url})` }} className='w-full h-full rounded-2xl bg-center bg-cover duration-500 shadowspec'></div>)}
--- src/pages/portfolio/Project.jsx
+++ src/pages/portfolio/Project.jsx
@@ -5,8 +5,11 @@ import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 //IMPORT COMPONENTS
 import Carousel from './Carousel';
-
-
+//IMPORT ICONS
+import { MdOutlineDescription } from 'react-icons/md'
+import { GrTechnology } from 'react-icons/gr'
+//IMPORT STYLES
+import './Project.css';
 
 
 
@@ -43,8 +46,43 @@ const Project = () => {
                 {project && (
                     <>
                     {isPending && <p className='text-[30px] text-center mt-[200px]'>Loading...</p>}
-                    {project && <h1 className='text-center text-[25px] text-primary mt-[80px]'>{project.name}</h1>}
-                    {project && <Carousel img={project.img}/>}
+                    {project && 
+                        <div className="w-[900px] mx-auto flex items-center justify-center">
+                            <div className="w-[30%] h-[5px] self-center bg-secondary "></div>
+                            <h1 className='text-center text-[35px] mt-[80px] text-primary'>{project.name}</h1>   
+                            <div className="w-[30%] h-[5px] self-center bg-secondary "></div>
+                        </div>}
+                    
+                    
+                    <div className="flex flex-col xl:flex-row items-center justify-center mt-[100px] mx-auto gap-[200px]">
+                        
+                
+                            {project && <Carousel img={project.img}/>}
+                       
+                        <div className="flex flex-col gap-[60px]">
+                            <div className="text-center flex flex-col items-center justify-center tracking-wider leading-7">
+                                <div className="flex">
+                                    <MdOutlineDescription className='text-center text-[30px]'/>
+                                    <h1 className='text-center text-[27px] ml-[30px]'>Description</h1>
+                                </div>
+                                <p className='text-left w-[400px] mt-[30px] '>
+                                    {project.descp}
+                                </p>   
+                            </div>
+                            <div className="text-center flex flex-col items-center justify-center tracking-wider leading-7">
+                                <div className="flex">
+                                    <GrTechnology className='text-center text-[30px]'/>
+                                    <h1 className='text-center text-[27px] ml-[30px]'>Features</h1>
+                                </div>
+                                <ul className='mt-[20px] text-left'>
+                                    {project.features.map(feat=>(
+                                            <li className='my-[10px]'>{feat}.</li>
+                                    ))}
+                                </ul> 
+                            </div>
+                        </div>
+
+                    </div>
                     </>
                 )}     
         </div>
